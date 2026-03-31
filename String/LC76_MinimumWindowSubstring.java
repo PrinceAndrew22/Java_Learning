d5ab0efed79b1156049bcfa4f2cee48c78677d7a
PrinceAndrew22 <princeandrew.0405@gmail.com>
2026-03-31 15:54:35 +0530

    Solve Solution for Minimum Window Substring

---
+++ String/LC76_MinimumWindowSubstring.java
@@ -0,0 +1,63 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.length() > s.length()) return "";
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0;
+        int count = t.length();
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+
+            if (map.containsKey(c)) {
+                if (map.get(c) > 0) count--;
+                map.put(c, map.get(c) - 1);
+            }
+            right++;
+
+            while (count == 0) {
+                if (right - left < minLen) {
+                    minLen = right - left;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+                if (map.containsKey(leftChar)) {
+                    map.put(leftChar, map.get(leftChar) + 1);
+                    if (map.get(leftChar) > 0) count++;
+                }
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+    }
+}
+
+public class LC76_MinimumWindowSubstring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string s: ");
+        String s = sc.nextLine();
+
+        System.out.print("Enter string t: ");
+        String t = sc.nextLine();
+
+        Solution sol = new Solution();
+        String result = sol.minWindow(s, t);
+
+        System.out.println("Minimum Window Substring: " + result);
+
+        sc.close();
+    }
+}
